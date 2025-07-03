d466b3b126dd727b4a87bdfeb2fd3069fd127b94
Daniel Billings <155379395+Eclipse7O7@users.noreply.github.com>
2025-07-03 17:31:15 +0100

    Github Testing

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        
+        Test for GitHub
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
